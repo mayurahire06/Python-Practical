9a4b34c8c2592cd3f33153615e20068a0dfb050d
mayurahire06 <mayurahire863@gmail.com>
2024-12-07 13:58:23 +0530

    updated

--- Assignment-3/2.py
+++ Assignment-3/2.py
@@ -1,7 +1,11 @@
 # Create a function that imports datetime and returns the current date and time.
 # Example: current_datetime() should return the current timestamp like "2023-09-14 15:30:25"
 
-import datetime
+from datetime import datetime
 
-current_time = datetime.datetime.now()
-print("Current date and time:", current_time)
+# Get the current date and time
+now = datetime.now()
+# Format it as "YYYY-MM-DD HH:MM:SS"
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
